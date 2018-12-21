df8bda43db716d9c58543003d7f356559567002c
benjaminJ <kamikillerto@gmail.com>
2018-12-21 15:23:21 +0100

    feat: Implement the service implementation switch at build time
    Replace the bugsnag service by the dummy one is current build env is not specified in the `notifyReleaseStages` option

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
+/* eslint-env node */
 'use strict';
+const Funnel = require('broccoli-funnel');
+const rename = require('broccoli-stew').rename;
+const map = require('broccoli-stew').map;
 
 module.exports = {
-  name: 'ember-cli-bugsnag-reporter'
+  name: 'ember-cli-bugsnag-reporter',
+
+  config(environment, appConfig) {
+    const options = appConfig.bugsnag || {};
+    options.notifyReleaseStages = options.notifyReleaseStages || [];
+    this.useDummyService = options.notifyReleaseStages.indexOf(environment) === -1;
+
+    appConfig.bugsnag = options;
+
+    return appConfig;
+  },
+
+  included() {
+    this._super.included.apply(this, arguments);
+  },
+  // Rename the service 'bugsnag-dummy' in 'bugsnag' if needed
+  treeFor(name) {
+    if (name === 'addon' || name === 'app') {
+      return map(this._super.treeFor.apply(this, arguments), (content) => {
+        return content.replace('/bugsnag-dummy', '/bugsnag');
+      });
+    }
+    return this._super.treeFor.apply(this, arguments);
+  },
+  // remove unwanted service depending on the value of `useDummyService`
+  treeForAddon: function() {
+    // see: https://github.com/ember-cli/ember-cli/issues/4463
+    let tree = this._super.treeForAddon.apply(this, arguments);
+
+    if (this.useDummyService === true) {
+      tree = new Funnel(tree, {
+        exclude: ['ember-cli-bugsnag-reporter/services/bugsnag.js']
+      });
+      tree = rename(tree, 'ember-cli-bugsnag-reporter/services/bugsnag-dummy.js', 'ember-cli-bugsnag-reporter/services/bugsnag.js');
+    } else {
+      tree = new Funnel(tree, {
+        exclude: ['ember-cli-bugsnag-reporter/services/bugsnag-dummy.js']
+      });
+    }
+    // var loggedApp = log(tree, { output: 'tree', label: 'my-app-name tree' });
+    return tree;
+  }
 };
