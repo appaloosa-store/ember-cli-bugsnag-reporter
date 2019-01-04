1a58d852c533c6b548c7374285f0e9027bfc7172
benjaminJ <kamikillerto@gmail.com>
2019-01-04 10:40:57 +0100

    fix: Don't import utils functions one by one but as a whote module

--- app/initializers/bugsnag.js
+++ app/initializers/bugsnag.js
@@ -1,12 +1,13 @@
 import config from '../config/environment';
-import { getUser, getMetaData } from '../utils/bugsnag';
+import * as bugsnag from '../utils/bugsnag';
+
 export function initialize() {
   const application = arguments[1] || arguments[0];
   const options = config['bugsnag-reporter'] || {};
   application.register('config:bugsnag', options, { instantiate: false });
   application.inject('service:bugsnag', 'options', 'config:bugsnag');
 
-  application.register('meta:bugsnag', { getUser, getMetaData }, { instantiate: false });
+  application.register('meta:bugsnag', { getUser: bugsnag.getUser, getMetaData: bugsnag.getMetaData }, { instantiate: false });
   application.inject('service:bugsnag', 'meta', 'meta:bugsnag');
 }
 
